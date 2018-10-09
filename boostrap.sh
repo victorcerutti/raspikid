d9c8bbfc4e9d824b64eb5e429b626bc0d5b140cb
Victor Cerutti <victor.cerutti@gmail.com>
2018-10-09 09:53:01 +0200

    launch script after boot

--- boostrap.sh
+++ boostrap.sh
@@ -8,3 +8,7 @@ sudo apt-get install python3-pygame
 #see http://python-omxplayer-wrapper.readthedocs.io/en/latest/
 sudo apt-get install -y libdbus-1{,-dev}
 pip3 install omxplayer-wrapper
+
+#launch script after boot
+sed -i "/.*raspikid-launch.*/d" ~/.bashrc
+echo 'cd ~/raspikid && python3 app.py #raspikid-launch' >> ~/.bashrc
