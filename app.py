3c591adeedf8b68bb5a589886b9ad0c5b62be19a
Victor Cerutti <victor.cerutti@gmail.com>
2019-02-17 09:19:40 +0000

    fordward, rewind, spaces

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ pygame.init()
 
 width = 1080
 height = 720
+#todo: try 1920 x 1080
 
 #Startup sound
 pygame.mixer.music.load('./sounds/startup_20.mp3')
@@ -34,79 +35,120 @@ listing = open("./movies.txt","r")
 
 while True:
 
-	barcode = input("Please scan your movie barcode... ") # waiting the barcode to be scanned and sent
+  barcode = input("Please scan your movie barcode... ") # waiting the barcode to be scanned and sent
 
-	logging.debug('Scanning '+barcode)
+  logging.debug('Scanning '+barcode)
 
-	if barcode == 'SHUTDOWN' :
-		print ('Shutting down...')
-		logging.info('Shutting down')
-		listing.close()
-		pygame.quit()
-		exit()
-		os.system('sudo shutdown -h now')
+  if barcode == 'SHUTDOWN' :
+    print ('Shutting down...')
+    logging.info('Shutting down')
+    listing.close()
+    pygame.quit()
+    os.system('sudo shutdown -h now')
+    exit()
 
-	elif barcode == 'EXIT' :
-		print ('Exiting...')
-		logging.info('Exiting')
-		listing.close()
-		pygame.quit()
-		exit()
+  elif barcode == 'EXIT' :
+    print ('Exiting...')
+    logging.info('Exiting')
+    listing.close()
+    pygame.quit()
+    exit()
 
-	elif barcode == 'PAUSE' :
-		print('Pausing video...')
-		logging.info('Pause')
-		try:
-			player.play_pause()
-		except :
-			print("")
+  elif barcode == 'PAUSE' :
+    print('Pausing video...')
+    logging.info('Pause')
+    try:
+      player.play_pause()
+      print("position "+player.position()+" sec")
+    except :
+      print("")
 
-	elif barcode == 'STOP' :
-		print('Stopping video...')
-		logging.info('Stop')
-		try:
-			player.quit()
-		except :
-			print("")
+  elif barcode == 'STOP' :
+    print('Stopping video...')
+    logging.info('Stop')
+    try:
+      print("position "+player.position()+" sec")
+      player.quit()
+    except :
+      print("")
+
+  elif barcode == '+10sec':
+    try:
+      player.seek(10)
+    except:
+      print("Error while seeking")
+
+  elif barcode == '-10sec':
+    try:
+      player.seek(-10)
+    except:
+      print("Error while seeking")
+
+  elif barcode == '+1min':
+    try:
+      player.seek(60)
+    except:
+      print("Error while seeking")
+
+  elif barcode == '-1min':
+    try:
+      player.seek(-60)
+    except:
+      print("Error while seeking")
+
+  elif barcode == '+10min':
+    try:
+      player.seek(600)
+    except:
+      print("Error while seeking")
+
+  elif barcode == '-10min':
+    try:
+      player.seek(-600)
+    except:
+      print("Error while seeking")
+
+  # Search the barcode in the listing
+  else:
+
+    #initialize vars
+    foundMovie = False
+    listing.seek(0)
+
+    for line in listing:
+
+      datas = line.rstrip('\n\r').split(",")
+
+      if datas[0] == barcode :
+
+        #./omxplayer -p -o hdmi test.mkv
+        print('Playing movie '+datas[1]);
+
+        foundMovie=True
+        logging.info('Playing movie '+barcode)
+
+        try:
+          player.quit()
+        except :
+          print("")
+
+        movie = ("./movies/"+datas[1])
+
+        try:
+          #OMXPlayer(movie, args='--pos 0:10:00')
+          #Start position (hh:mm:ss)
+          player = OMXPlayer(movie)
+        except:
+          print("Error while playing movie")
+
+        #set_position
 
 
-	# Search the barcode in the listing
-	else:
-
-		#initialize vars
-		foundMovie = False
-		listing.seek(0)
-
-		for line in listing:
-
-			datas = line.rstrip('\n\r').split(",")
-
-			if datas[0] == barcode :
-
-                #./omxplayer -p -o hdmi test.mkv
-				print('Playing movie '+datas[1]);
-
-				foundMovie=True
-				logging.info('Playing movie '+barcode)
-
-				try:
-					player.quit()
-				except :
-					print("")
-
-				movie = ("./movies/"+datas[1])
-
-				try:
-					player = OMXPlayer(movie)
-				except:
-					print("Error while playing movie")
-
-
-		# no available barcode
-		if foundMovie == False :
-			pygame.mixer.music.load('./sounds/sad_trombone.mp3')
-			pygame.mixer.music.play(0)
-			print('Error with barcode')
+    # no available barcode
+    if foundMovie == False :
+      pygame.mixer.music.load('./sounds/sad_trombone.mp3')
+      pygame.mixer.music.play(0)
+      print('Error with barcode')
 
 
 listing.close()
